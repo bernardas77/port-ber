7bc037b2cc6c83313242b13bded1e4728ff15437
unknown <bernardas77@gmail.com>
2023-09-02 14:39:44 +0300

    changed react-typed to react-simple-typewriter package

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import HeroImage from '../assets/heroimage.jpg'
 import {MdKeyboardArrowRight} from 'react-icons/md'
-// import Typed from 'react-typed';
+import { useTypewriter, Cursor} from 'react-simple-typewriter';
 import { Link } from 'react-scroll'
 
 const Home = () => {
+
+  const [text] = useTypewriter({
+    words: ['Mokinys', 'Studentas', 'Programuotojas'],
+    loop: {}
+  })
+
   return (
     <div name='home' className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 pt-14 md:pt-0'>
       <div className='max-w-md md:max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row pt-10 md:pt-0'>
         <div className='flex flex-col justify-center items-center text-center'>
           <div className='flex flex-row'>
-            {/* <Typed className='break-all text-4xl md:text-5xl lg:text-7xl font-bold text-white' strings={['Mokinys', 'Studentas', 'Programuotojas']} typeSpeed={90} backSpeed={90} backDelay={1500} loop/> */}
+            <p className='break-all text-4xl md:text-5xl lg:text-7xl font-bold text-white'>{text}</p>
+            <span className='text-4xl md:text-5xl lg:text-7xl font-bold text-white'>
+              <Cursor />
+            </span>
           </div>
           
           <p className='text-gray-300 pt-3 md:pt-6 pb-4 w-[100%] md:w-[90%] text-[15px] md:text-[20px]'>
